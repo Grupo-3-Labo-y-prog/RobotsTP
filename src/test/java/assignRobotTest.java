0070bb92aa18e08c76d24cd7bd9c23ca3aa06da4
matramoka <matramoka@gmail.com>
2022-05-09 22:25:49 -0300

    Test del punto 3.

--- src/test/java/assignRobotTest.java
+++ src/test/java/assignRobotTest.java
@@ -13,8 +13,7 @@ class assignRobotTest {
     HashSet<Robot> robotsAssigned;
     AssignRobot aasignRobot;
 
-    //punto 2 Un cliente Classic, sin deuda, realiza un pedido de limpieza y ordenamiento. Se le asigna el robot
-    //K311Y-fl y el robot S031RTY ya que el K311Y-a es más costoso. (OJO! Cambiamos K311Y-fl por el K311Y-fu porque esa será la opcion mas economica)
+
     @BeforeEach
     void setUp(){
         this.client = new Client(1111, new Classic(),0, 0);
@@ -38,9 +37,12 @@ class assignRobotTest {
 
     }
 
-
+    //punto 2 Un cliente Classic, sin deuda, realiza un pedido de
+    // limpieza y ordenamiento. Se le asigna el robot
+    //K311Y-fl y el robot S031RTY ya que el K311Y-a es más costoso.
+    // (OJO! Cambiamos K311Y-fl por el K311Y-fu porque esa será la opcion mas economica)
     @Test
-    void assignRobot (){
+    void assignRobotMoreEconomicToClassicClientCleanningOrdering (){
         ArrayList<Tasks> tasks = new ArrayList<>();
         tasks.add(Tasks.CLEANNING);
         tasks.add(Tasks.ORDERING);
@@ -54,7 +56,27 @@ class assignRobotTest {
 
         Request request = new Request(1,this.client, tasks, new Simple(), "Libertador 2231, Olivos");
 
+        assertEquals(expected,this.aasignRobot.assignation(request));
+    }
+    //Punto 3. Un cliente Classic, sin deuda, realiza un pedido de limpieza y
+    // lustrado de muebles. Se le asigna el robot K311Y-fl y el robot K311Y-fu.
+    // (OJITO! Sulo debería asignar el robot K311fu, puede hacer ambas tareas y es el mas economico)
+    @Test
+     void assignRobotMoreEconomicToClassicClientCleanningPolishing (){
+        ArrayList<Tasks> tasks = new ArrayList<>();
+        tasks.add(Tasks.CLEANNING);
+        tasks.add(Tasks.POLISHING);
 
-        assertTrue(this.aasignRobot.assignation(request).contains(K311Yfu));
+        Robot K311Yfu = new K311Yfu();
+
+
+        HashSet<Robot> expected = new HashSet<>();
+        expected.add(K311Yfu);
+
+
+        Request request = new Request(8,this.client, tasks, new Complex(), "Emilio Frers 2048, Martinez");
+
+        assertEquals(expected, this.aasignRobot.assignation(request));
+
     }
 }
