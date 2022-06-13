2c7ef53a74cf577791fc38376f4fc339b2a3dd07
RitaDaviss <102768004+RitaDaviss@users.noreply.github.com>
2022-06-13 12:26:55 -0300

    Clases para Reparaciones

---
+++ src/main/java/Services/Gas.java
@@ -0,0 +1,27 @@
+package Services;
+
+import Entities.Employee;
+
+public class Gas extends Service{
+
+    public Gas(int complexity, Employee employee) {
+        super(complexity, employee);
+    }
+
+    @Override
+    public float setCost() {
+        float value = 0;
+        if (super.getComplexity() < 3){
+            value = 1000;
+        }
+        else if (super.getComplexity() <= 6){
+            value = 3530;
+        }
+        else {
+            value = 6389;
+        }
+        return value;
+    }
+
+
+}
